b6341fde5852f74f560484998bb3c52f375d6629
Michael Gfeller <michael.gfeller@ost.ch>
2021-04-08 09:57:04 +0200

    Init

---
+++ src/components/classes.js
@@ -0,0 +1,9 @@
+export default [
+    'Alain Berset',
+    'Guy Parmelin',
+    'Ignazio Cassis',
+    'Karin Keller-Sutter',
+    'Simonetta Sommaruga',
+    'Ueli Maurer',
+    'Viola Amherd',
+];
